92b243417d7666803fb4606456ab785e92c48c71
seppe-ado <seppe.ado.vandewalle@gmail.com>
2023-06-07 11:31:00 +0200

    Final Update

--- src/main/java/com/example/seppe_vandewalle/Model/DAO/ProductsDAO.java
+++ src/main/java/com/example/seppe_vandewalle/Model/DAO/ProductsDAO.java
@@ -3,10 +3,10 @@ package com.example.seppe_vandewalle.Model.DAO;
 import com.example.seppe_vandewalle.Model.Entities.Products;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+
 
 public interface ProductsDAO extends CrudRepository<Products, Integer> {
 
-//List<Products> findProductsById();
+
 
 }
--- src/main/java/com/example/seppe_vandewalle/Model/Entities/Products.java
+++ src/main/java/com/example/seppe_vandewalle/Model/Entities/Products.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 
 
 @Entity
